aed4774b40ec4bf526de436c4e12e7c5cccc0a08
Mirshakirov <mirsaid@Mirsaids-MacBook-Pro.local>
2025-10-23 20:35:04 +0500

    Empty commit for branch 10

--- src/main/java/lessons/lesson_10/Something.java
+++ src/main/java/lessons/lesson_10/Something.java
@@ -2,8 +2,10 @@ package lessons.lesson_10;
 
 public class Something {
     public static void main(String[] args) {
-        for (int i = 0; i < 5; i++) {
-            System.out.println(i);
+        int i;
+        for (i = 0; i < 5; i++) {
+
         }
+        System.out.println(i);
     }
 }
